dd285fa4125e28b578985991141a712caf64783a
shivanid2003 <shivanid7281@gmail.com>
2026-04-30 18:31:04 +0530

    Secure Google Maps API key and stop tracking .env file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,8 +8,22 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBS8jy1qJHMoQeT9jyH4dtyxM_x8uKLxuY")
+    if let path = Bundle.main.path(forResource: "flutter_assets/.env", ofType: nil) {
+        do {
+            let content = try String(contentsOfFile: path, encoding: .utf8)
+            let lines = content.components(separatedBy: .newlines)
+            for line in lines {
+                if line.starts(with: "GOOGLE_MAPS_API_KEY=") {
+                    let key = line.replacingOccurrences(of: "GOOGLE_MAPS_API_KEY=", with: "").trimmingCharacters(in: .whitespacesAndNewlines)
+                    GMSServices.provideAPIKey(key)
+                }
+            }
+        } catch {
+            print("Error reading .env file: \(error)")
+        }
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
 }
